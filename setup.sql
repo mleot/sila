b4f7c45dfdbf1dc675a6dd65e37319d59ed3ba87
Sean Morris <sean.morris@snowflake.com>
2024-12-21 10:42:43 -0700

    Create setup.sql

---
+++ setup.sql
@@ -0,0 +1,51 @@
+-- SET ROLE CONTEXT
+USE ROLE accountadmin;
+
+-- SET DATABASE SCHEMA CONTEXT
+CREATE DATABASE IF NOT EXISTS sila_demo_db;
+USE DATABASE sila_demo_db;
+CREATE SCHEMA IF NOT EXISTS sila_demo_db.notebooks
+USE SCHEMA sila_demo_db.notebooks;
+
+-- SET WAREHOUSE CONTEXT
+CREATE WAREHOUSE IF NOT EXISTS sila_demo_wh 
+    WAREHOUSE_SIZE = xsmall
+    AUTO_SUSPEND = 60;
+USE WAREHOUSE sila_demo_wh;
+
+-- CREATE GIT SECRET
+CREATE SECRET IF NOT EXISTS github_secret
+    TYPE = password
+    USERNAME = '<Github Username>' 
+    PASSWORD = '<Personal Access Token (classic)>'; 
+
+-- CREATE API INTEGRATION
+CREATE API INTEGRATION IF NOT EXISTS git_api_integration
+    API_PROVIDER = git_https_api
+    API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-smorris/') 
+    ALLOWED_AUTHENTICATION_SECRETS = (github_secret)
+    ENABLED = true;
+
+-- CREATE GIT REPOSITORY
+CREATE GIT REPOSITORY IF NOT EXISTS sila_repo
+    API_INTEGRATION = git_api_integration
+    GIT_CREDENTIALS = github_secret
+    ORIGIN = 'https://github.com/sfc-gh-smorris/sila';
+
+-- SHOW REPO
+SHOW git repositories;
+
+-- FETCH REPO UPDATES
+ALTER git repository sila_repo FETCH;
+
+-- SHOW BRANCHES
+SHOW git branches IN git repository sila_repo;
+
+-- LIST FILES
+LIST @sila_repo/branches/main;
+
+-- SHOW CODE IN FILE
+SELECT $1 FROM @sila_repo/branches/main/create_notebook.sql;
+
+-- GRANT TO SYSADMIN
+EXECUTE IMMEDIATE FROM @sila_repo/branches/main/create_notebook.sql;
