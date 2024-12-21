6c37e436bff62ab1d408c91089198474d4c0f4f5
Sean Morris <sean.morris@snowflake.com>
2024-12-21 10:38:37 -0700

    Update and rename setup.sql to create_notebook.sql

--- create_notebook.sql
+++ create_notebook.sql
@@ -14,34 +14,53 @@ CREATE COMPUTE POOL IF NOT EXISTS cpu_xs_5_nodes
     INSTANCE_FAMILY = CPU_X64_XS;
 
 -- CREATE NETWORK RULE
-CREATE NETWORK RULE allow_all_rule
+CREATE NETWORK RULE IF NOT EXISTS allow_all_rule
     TYPE = 'HOST_PORT'
     MODE= 'EGRESS'
     VALUE_LIST = ('0.0.0.0:443','0.0.0.0:80');
 
 -- CREATE EXTERNAL ACCESS INTEGRATION
-CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION allow_all_integration
+CREATE EXTERNAL ACCESS INTEGRATION IF NOT EXISTS allow_all_integration
     ALLOWED_NETWORK_RULES = (allow_all_rule)
     ENABLED = true;
 
 -- CREATE NETWORK RULE
-CREATE OR REPLACE NETWORK RULE pypi_network_rule
+CREATE NETWORK RULE IF NOT EXISTS pypi_network_rule
   MODE = EGRESS
   TYPE = HOST_PORT
   VALUE_LIST = ('pypi.org', 'pypi.python.org', 'pythonhosted.org',  'files.pythonhosted.org');
 
 -- CREATE EXTERNAL ACCESS INTEGRATION
-CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION pypi_access_integration
+CREATE EXTERNAL ACCESS INTEGRATION IF NOT EXISTS pypi_access_integration
   ALLOWED_NETWORK_RULES = (pypi_network_rule)
   ENABLED = true;
 
-GRANT ALL ON WAREHOUSE sila_demo_wh TO ROLE sysadmin;
-GRANT USAGE ON COMPUTE POOL cpu_xs_5_nodes TO ROLE sysadmin;
-GRANT USAGE ON COMPUTE POOL gpu_s_5_nodes TO ROLE sysadmin;
+-- GRANT TO SYSADMIN
 GRANT USAGE ON DATABASE sila_demo_db TO ROLE sysadmin;
 GRANT ALL ON SCHEMA sila_demo_db.notebooks TO ROLE sysadmin;
 GRANT CREATE STAGE ON SCHEMA sila_demo_db.notebooks TO ROLE sysadmin;
 GRANT CREATE NOTEBOOK ON SCHEMA sila_demo_db.notebooks TO ROLE sysadmin;
 GRANT CREATE SERVICE ON SCHEMA sila_demo_db.notebooks TO ROLE sysadmin;
+GRANT ALL ON WAREHOUSE sila_demo_wh TO ROLE sysadmin;
+GRANT READ, WRITE ON GIT REPOSITORY sila_demo_db.notebooks.sila_repo TO ROLE sysadmin;
+GRANT USAGE ON COMPUTE POOL cpu_xs_5_nodes TO ROLE sysadmin;
+GRANT USAGE ON COMPUTE POOL gpu_s_5_nodes TO ROLE sysadmin;
 GRANT USAGE ON INTEGRATION allow_all_integration TO ROLE sysadmin;
 GRANT USAGE ON INTEGRATION pypi_access_integration TO ROLE sysadmin;
+
+-- SET ROLE CONTEXT
+USE ROLE sysadmin;
+
+-- SET DATABASE SCHEMA CONTEXT
+USE SCHEMA sila_demo_db.notebooks;
+
+-- SET WAREHOUSE CONTEXT
+USE WAREHOUSE sila_demo_wh;
+
+-- CREATE NOTEBOOK
+CREATE NOTEBOOK IF NOT EXISTS sila_demo_db.notebooks.pybamm
+FROM '@sila_repo/branches/main'
+    MAIN_FILE = 'pybamm.ipynb' 
+    QUERY_WAREHOUSE = sila_demo_wh
+    RUNTIME_NAME = 'SYSTEM$BASIC_RUNTIME' 
+    COMPUTE_POOL = 'CPU_XS_5_NODES';
